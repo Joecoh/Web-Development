32b02ddf9c92f2ed4acd19e3ae62b8d713ba79bb
Joash Cohen <103135943+Joecoh@users.noreply.github.com>
2025-03-18 21:36:32 +0530

    Create action_page.php

---
+++ hidden field ex5b/action_page.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Result</title>
+</head>
+<body>
+<h4>Hidden Value:</h4>
+<?php echo $_GET["custId"]; ?><br>
+<h4>First Name:</h4>
+<?php echo $_GET["fname"]; ?><br>
+</body>
+</html>
